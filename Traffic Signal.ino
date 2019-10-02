a22757ef04c5d38e182d99604b94d8f8ac41330c
Govind <52847415+govgoel@users.noreply.github.com>
2019-10-02 16:40:56 +0530

    Create Traffic Signal.ino

---
+++ Traffic Signal.ino
@@ -0,0 +1,33 @@
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop(){
+    changeLights();
+    delay(15000);
+}
+
+void changeLights(){
+    // green off, yellow on for 3 seconds
+    digitalWrite(11, LOW);
+    digitalWrite(13, HIGH);
+    delay(3000);
+
+    // turn off yellow, then turn red on for 5 seconds
+    digitalWrite(13, LOW);
+    digitalWrite(9, HIGH);
+    delay(5000);
+
+    // red and yellow on for 2 seconds (red is already on though)
+    digitalWrite(13, HIGH);
+    delay(2000);
+
+    // turn off red and yellow, then turn on green
+    digitalWrite(9, LOW);
+    digitalWrite(13, LOW);
+    digitalWrite(11, HIGH);
+    delay(3000);
+}
